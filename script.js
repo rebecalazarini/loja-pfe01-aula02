e03d75925d6e2203f8c5b57b3f7916b95c3688b1
rebecalazarini <rebecalazarini2007@gmail.com>
2025-03-19 14:29:45 -0300

    Atualizações em todos os arquivos

--- script.js
+++ script.js
@@ -4,12 +4,19 @@ var produtos = [];
 //Carrega os dados do Mockup
 fetch(uri)
     .then(resp => resp.json())
-    .then(resp =>{produtos = resp;})
+    .then(resp => { produtos = resp; })
     .then(() => mostrarCards())
 
-function mostrarCards(){
+function mostrarCards() {
     const main = document.querySelector('main');
-    produtos.forEach(p=>{
-        main.innerHTML += p.produto; 
+    produtos.forEach(p => {
+        main.innerHTML += `
+        <div class="card">
+            <h3>${p.produto}</h3>
+            <img src="${p.imagem}" alt="${p.produto}">
+            <p>Preco: ${p.preco}</p>
+            <button>Detalhes</button>
+        </div>
+        `;
     })
 }
