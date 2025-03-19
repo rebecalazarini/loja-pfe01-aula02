19314d7ce81eaa8364c7832357fba000228f6568
rebecalazarini <rebecalazarini2007@gmail.com>
2025-03-19 14:16:26 -0300

    inicio do script

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('oi');
+const uri = './assets/dados.json';
+var produtos = [];
+
+//Carrega os dados do Mockup
+fetch(uri)
+    .then(resp => resp.json())
+    .then(resp =>{produtos = resp;})
+    .then(() => mostrarCards())
+
+function mostrarCards(){
+    const main = document.querySelector('main');
+    produtos.forEach(p=>{
+        main.innerHTML += p.produto; 
+    })
+}
